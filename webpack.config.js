de20b62bfca2a2802e788bb1091313c5dedf21c8
rchaser53 <tayoshizawa29@gmail.com>
2017-11-03 23:46:08 +0900

    try to use CommonsChunkPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,14 @@ module.exports = {
 	context: path.resolve(__dirname, './src'),
   devtool: 'inline-source-map',
   entry: {
-    index: "./index.js"
+    index1: "./index1.js",
+    index2: "./index2.js",
+    index3: "./index3.js",
   },
   output: {
     path: path.resolve(__dirname, "dist"),
     publicPath: "/",
-    filename: "bundle.js"
+    filename: "[name].js"
   },
   resolve: {
       extensions: ['.html', '.js']
@@ -26,7 +28,11 @@ module.exports = {
   }]},
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      chunks: ['index1', 'index2'],
+    })
   ],
   externals: [
     { Vue: true }
